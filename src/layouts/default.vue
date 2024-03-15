0159e839ff039de4535c2a61b010e6c6ddeba1a9
Alan Tavares Morais <pollargithub@gmail.com>
2024-03-15 07:56:36 -0300

    ajuste de favicon

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
     <Head>
-        <Link rel="icon" type="image/svg+xml" href="/images/elekro-icon.svg"/>
         <Link rel="stylesheet" href="/css/font.css"/>
     </Head>
     <HeaderDefault/>
