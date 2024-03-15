129e19288c225249136ebef25cb015481d59fd06
Alan Tavares Morais <pollargithub@gmail.com>
2024-03-15 07:54:32 -0300

    troca de favicon e recriação do footer

--- src/components/footer-default.vue
+++ src/components/footer-default.vue
@@ -1,48 +1,25 @@
 <template>
     <footer class="bg-black text-white">
         <div class="container mx-auto px-5">
-            <div class="grid grid-cols-1 lg:grid-cols-4 gap-3 py-11 lg:py-[100px]">
-                <div class="contact-container">
-                    <LogoTipo/>
-                </div>
-
-                <div class="contact-container">
-                    <h6>telefone:</h6>
-                    <a href="javascript:void(0)" target="_blank">(00) 00000-0000</a>
-                </div>
-
-                <div class="contact-container">
-                    <h6>whatsapp:</h6>
-                    <a href="javascript:void(0)" target="_blank">(00) 00000-0000</a>
-                </div>
-
-                <div class="contact-container">
-                    <h6>e-mail:</h6>
-                    <a href="mailto:vendas@elekro.com.br?subject=Gostaria de saber mais sobre o produto" target="_blank">vendas@elekro.com.br</a>
-                </div>
+            <div class="text-center pt-11 pb-6">
+                <figure class="mb-3">
+                    <img class="max-w-full inline-block" src="/images/logo-icon.png" alt="logo" srcset="/images/logo-icon.png">
+                </figure>
+                <p class="text-[#aaaaaa] font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <div class="py-11 lg:py-[100px] flex flex-col md:flex-row md:items-center md:justify-between border-t-[1px] border-[#1d1d1d] gap-y-3">
-                <span>&copy; 2024 <a class="hover:text-[#09aff4] duration-200 uppercase font-medium text-sm inline-block" target="_blank" href="https://logn.com.br/servicos">logn.</a> todos os direitos reservados</span>
-                <ul class="flex gap-4">
-                    <template v-for="midia, index in midiasSociais" :key="index">
-                        <li class="text-xl md:text-2xl hover:text-[#aaaaaa] duration-200">
-                            <a href="javascript:void(0)" target="_blank" :title="midia.title">
-                                <Icon :name="midia.icon"/>
-                            </a>
-                        </li>
-                    </template>
-                </ul>
+            <div class="pb-11 pt-6 text-center border-t-[1px] border-[#1d1d1d]">
+                <span class="font-light inline-block">&copy; 2024 <a class="hover:text-[#09aff4] duration-200 uppercase font-medium text-sm inline-block" target="_blank" href="https://logn.com.br/servicos">logn.</a></span>
             </div>
         </div>
     </footer>
 </template>
 
 <script setup lang="ts">
-import { midiasSociais } from "~/constants";
+// import { midiasSociais } from "~/constants";
 </script>
 
 <style scoped>
-.social-midia{
+/* .social-midia{
     @apply flex items-start justify-center gap-3 sm:order-last col-span-3 md:col-span-1
 }
 
@@ -58,5 +35,5 @@ import { midiasSociais } from "~/constants";
 
 .contact-container > a{
     @apply font-light text-xl inline-block;
-}
+} */
 </style>~/app/constants
