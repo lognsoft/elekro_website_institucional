1571e5726f7dda5578f54eebbadcbce79cc52a17
Alan Tavares Morais <pollargithub@gmail.com>
2024-03-16 17:15:59 -0300

    alteranção da ordem de elementos do footer

--- src/components/footer-default.vue
+++ src/components/footer-default.vue
@@ -1,34 +1,31 @@
 <template>
     <footer class="bg-black text-white">
         <div class="container mx-auto px-5">
-            <div class="text-center pt-11 pb-6">
+            <div class="pb-11 pt-6 text-center stores">
+                <a title="Play Store" href="javascript:void(0)">
+                    <span class="icon-store">
+                        <Icon name="simple-icons:googleplay"/>
+                    </span>
+                    <span class="block capitalize">play store</span>
+                </a>
+                <a title="Apple Store" href="javascript:void(0)">
+                    <span class="icon-store">
+                        <Icon name="grommet-icons:apple-app-store"/>
+                    </span>
+                    <span class="block capitalize">apple store</span>
+                </a>
+                <a title="Linkedin" href="https://www.linkedin.com/posts/elekro-best-price_parceiro-elekro-activity-7165984652757868544-Pol6?utm_source=share&utm_medium=member_ios" target="_blank">
+                    <span class="icon-store">
+                        <Icon name="jam:linkedin-square"/>
+                    </span>
+                    <span class="block capitalize">linkedin</span>
+                </a>
+            </div>
+            <div class="text-center pt-11 pb-6 border-t-[1px] border-[#1d1d1d]">
                 <figure class="mb-3">
                     <img class="max-w-full inline-block" src="/images/logo-icon.png" alt="logo" srcset="/images/logo-icon.png">
                 </figure>
                 <p class="text-[#aaaaaa] font-light mb-4 text-sm">Copyright Elekro Best Price do Brasil LTDA © 2024 — Todos os direitos reservados.</p>
-                <div class="stores">
-                    <a title="Play Store" href="javascript:void(0)">
-                        <span class="icon-store">
-                            <Icon name="simple-icons:googleplay"/>
-                        </span>
-                        <span class="block capitalize">play store</span>
-                    </a>
-                    <a title="Apple Store" href="javascript:void(0)">
-                        <span class="icon-store">
-                            <Icon name="grommet-icons:apple-app-store"/>
-                        </span>
-                        <span class="block capitalize">apple store</span>
-                    </a>
-                    <a title="Linkedin" href="https://www.linkedin.com/posts/elekro-best-price_parceiro-elekro-activity-7165984652757868544-Pol6?utm_source=share&utm_medium=member_ios" target="_blank">
-                        <span class="icon-store">
-                            <Icon name="jam:linkedin-square"/>
-                        </span>
-                        <span class="block capitalize">linkedin</span>
-                    </a>
-                </div>
-            </div>
-            <div class="pb-11 pt-6 text-center border-t-[1px] border-[#1d1d1d]">
-                <span class="font-light inline-block">&copy; 2024 Elekro</span>
             </div>
         </div>
     </footer>
