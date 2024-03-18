aaf25d4d5e4da8d92a11708be19d88827c2f133f
Alan Tavares Morais <pollargithub@gmail.com>
2024-03-18 17:39:11 -0300

    ajustes de botão de aplicativos

--- src/components/footer-default.vue
+++ src/components/footer-default.vue
@@ -2,45 +2,22 @@
     <footer class="bg-black text-white">
         <div class="container mx-auto px-5">
             <div class="pt-6 pb-3 stores">
-                <a title="Play Store" href="javascript:void(0)" target="_blank">
-                    <div class="icon-store">
-                        <Icon name="simple-icons:googleplay"/>
-                    </div>
-                    <div>
-                        <div class="uppercase text-[10px]">disponível na</div>
-                        <div class="block capitalize text-xl">play store</div>
-                    </div>
-                </a>
-                <a title="Apple Store" href="javascript:void(0)" target="_blank">
-                    <div class="icon-store">
-                        <Icon name="uiw:apple"/>
-                    </div>
-                    <div>
-                        <div class="uppercase text-[10px]">disponível na</div>
-                        <div class="block capitalize text-xl">app store</div>
-                    </div>
-                </a>
-                <a title="Linkedin" href="https://www.linkedin.com/posts/elekro-best-price_parceiro-elekro-activity-7165984652757868544-Pol6?utm_source=share&utm_medium=member_ios" target="_blank">
-                    <div class="icon-store">
-                        <Icon name="fontisto:linkedin"/>
-                    </div>
-                    <div>
-                        <div class="uppercase text-[10px]">encontre-nos no</div>
-                        <div class="block capitalize text-xl">linkedin</div>
-                    </div>
-                </a>
-                <!-- <a title="Apple Store" href="javascript:void(0)">
-                    <span class="icon-store">
-                        <Icon name="uiw:apple"/>
-                    </span>
-                    <span class="block capitalize">apple store</span>
-                </a> -->
-                <!-- <a title="Linkedin" href="https://www.linkedin.com/posts/elekro-best-price_parceiro-elekro-activity-7165984652757868544-Pol6?utm_source=share&utm_medium=member_ios" target="_blank">
-                    <span class="icon-store">
-                        <Icon name="jam:linkedin-square"/>
-                    </span>
-                    <span class="block capitalize">linkedin</span>
-                </a> -->
+
+
+                <template v-for="link,index in links" :key="index">
+                    <a class="col-span-1" :title="link.store" :href="link.href" target="_blank">
+                        <div class="icon-store">
+                            <Icon :name="link.icon"/>
+                        </div>
+                        <div>
+                            <div class="uppercase text-[8px]">{{ link.sub }}</div>
+                            <div class="block capitalize text-lg">{{ link.store }}</div>
+                        </div>
+                    </a>
+                </template>
+
+
+
             </div>
             <div class="text-center pb-6 pt-3 border-t-[1px] border-[#1d1d1d]">
                 <figure class="mb-1">
@@ -54,12 +31,37 @@
 </template>
 
 <script setup lang="ts">
-// import { midiasSociais } from "~/constants";
+type Link = {
+    sub:string,
+    store:string,
+    icon:string,
+    href:string,
+}
+const links:Link[] = [
+    {
+        sub:'disponível na',
+        store:'play store',
+        icon:'simple-icons:googleplay',
+        href:'javascript:void(0)',
+    },
+    {
+        sub:'disponível na',
+        store:'app store',
+        icon:'uiw:apple',
+        href:'javascript:void(0)',
+    },
+    {
+        sub:'encontre-nos no',
+        store:'linkedin',
+        icon:'fontisto:linkedin',
+        href:'https://www.linkedin.com/posts/elekro-best-price_parceiro-elekro-activity-7165984652757868544-Pol6?utm_source=share&utm_medium=member_ios',
+    }
+]
 </script>
 
 <style scoped>
 .stores{
-    @apply w-full max-w-[900px] flex flex-col md:flex-row items-center justify-center gap-3 md:gap-5 mx-auto
+    @apply w-full max-w-[200px] sm:max-w-[500px] grid grid-cols-1 sm:grid-cols-3 items-center justify-center gap-2 mx-auto
 }
 
 .stores a{
