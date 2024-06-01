b30a0f3bd5e06e9c95d500f141e8f6174efafdf9
Alan Tavares Morais <pollargithub@gmail.com>
2024-06-01 13:13:42 -0300

    página de desenvolvimento

--- src/pages/dev/index.vue
+++ src/pages/dev/index.vue
@@ -5,7 +5,10 @@ definePageMeta({
 </script>
 
 <template>
-    <main class="h-screen flex items-center justify-center px-3">
-        <h1 class="text-black text-4xl">Em Desenvolvimento</h1>
+    <main class="h-screen flex items-center justify-center px-3 bg-black text-white">
+        <div>
+            <img src="/dev.png" alt=""/>
+            <h1 class="text-4xl text-center">Estamos em construção</h1>
+        </div>
     </main>
 </template>
