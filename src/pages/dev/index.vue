bdbf2f97faee607e93b9bc0827265ddabe24d3dc
Alan Tavares Morais <pollargithub@gmail.com>
2024-06-04 18:21:44 -0300

    página dev1

--- src/pages/dev/index.vue
+++ src/pages/dev/index.vue
@@ -7,7 +7,7 @@ definePageMeta({
 <template>
     <main class="h-screen flex items-center justify-center bg-black">
         <div>
-            <img class="w-full min-h-[370px] md:h-screen object-center object-cover" src="/dev.png" alt=""/>
+            <img class="w-full min-h-[340px] md:h-screen object-center object-cover" src="/dev.png" alt=""/>
         </div>
     </main>
 </template>
