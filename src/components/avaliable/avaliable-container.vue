29e9bccde6e7f4643cfed8140e4b51dc6ca823da
Alan Tavares <pollargithub@gmail.com>
2025-06-08 09:57:06 -0300

    correção de textos de avaliações

--- src/components/avaliable/avaliable-container.vue
+++ src/components/avaliable/avaliable-container.vue
@@ -4,47 +4,51 @@
   const swiperRef = ref<SwiperClass | null>(null)
 
   const avaliables:IAvaliableCard[] = [
-    {
-      name:"Vinícius Oliveira",
-      avaliable: ["Produto excelente e a qualidade do serviço prestado ainda melhor. Tive todo suporte durante o período de pós compra/instalação. Recomendo muito a experiência com a elekro"],
-      stars:5
-    },
-    {
-      name: "Evelyn Werdine",
-      avaliable:["Ótima qualidade e designer sofisticadp. A facilidade do app é um diferencial."],
-      stars: 5
-    },
-    {
-      name: "Rony Neri",
-      avaliable: [
-        "Instalar a fechadura digital da Elekro foi uma excelente escolha! A fechadura tecnológica e segura combina biometria, chaves eletrônicas e conectividade Wi-Fi com um design moderno que fica muito bonito na porta.",
-        "Aproveito para parabenizar a equipe de Londrina-PR, profissionais extremamente atenciosos e competentes desde as primeiras interações até a instalação rápida e cuidadosa.",
-        "Recomendo muito para quem busca tecnologia e praticidade no dia a dia - e odeia ficar carregando chaves de casa de lado para o outro!"
-      ],
-      stars: 5
-    },
-    {
-      name: "Scau Zhang",
-      avaliable: [
-        "Esta fechadura é incrível, perfeita para min e minha familia. Acabamos de nos mudar para o Brasil e a equipe da Elekro nos deu todo suporte que precisávamos para manter nosa casa tecnológica e segura. Todo o processo foi muito rápido, desde a compra"
-      ],
-      stars: 5
-    },
-    {
-      name: "Laura Lima",
-      avaliable: [
-        "Atendimento impecável, melhor fechadura do mercado, não tenho o que reclamar, desde a pré venda, até o pós venda. Sempre muito atenciosos e prestativos com qualquer dúvida."
-      ],
-      stars: 5
-    },
-    {
-      name: "Marilyn Sandyla",
-      avaliable: [
-        "Excelente atendimento e serviço de alta qualidade! Desde o primeiro contato, fui muito bem atendida pela equipe, que demonstrou profissionalismo e dedicação, O ambiente é agradável, os produtos são de primeira qualidade e a experiência foi impecável. Recomendo a todos."
-      ],
-      stars: 5
-    }
-  ]
+  {
+    "name": "Vinícius Oliveira",
+    "avaliable": [
+      "Produto excelente e a qualidade do serviço prestado ainda melhor. Tive todo o suporte durante o período de pós-compra/instalação. Recomendo muito a experiência com a Elekro."
+    ],
+    "stars": 5
+  },
+  {
+    "name": "Evelyn Werdine",
+    "avaliable": [
+      "Ótima qualidade e design sofisticado. A facilidade do app é um diferencial."
+    ],
+    "stars": 5
+  },
+  {
+    "name": "Rony Neri",
+    "avaliable": [
+      "Instalar a fechadura digital da Elekro foi uma excelente escolha! A fechadura tecnológica e segura combina biometria, chaves eletrônicas e conectividade Wi-Fi com um design moderno que fica muito bonito na porta.",
+      "Aproveito para parabenizar a equipe de Londrina-PR, profissionais extremamente atenciosos e competentes desde as primeiras interações até a instalação rápida e cuidadosa.",
+      "Recomendo muito para quem busca tecnologia e praticidade no dia a dia — e odeia ficar carregando chaves de casa de um lado para o outro!"
+    ],
+    "stars": 5
+  },
+  {
+    "name": "Scau Zhang",
+    "avaliable": [
+      "Esta fechadura é incrível, perfeita para mim e minha família. Acabamos de nos mudar para o Brasil e a equipe da Elekro nos deu todo o suporte que precisávamos para manter nossa casa tecnológica e segura. Todo o processo foi muito rápido, desde a compra."
+    ],
+    "stars": 5
+  },
+  {
+    "name": "Laura Lima",
+    "avaliable": [
+      "Atendimento impecável, melhor fechadura do mercado. Não tenho o que reclamar, desde a pré-venda até o pós-venda. Sempre muito atenciosos e prestativos com qualquer dúvida."
+    ],
+    "stars": 5
+  },
+  {
+    "name": "Marilyn Sandyla",
+    "avaliable": [
+      "Excelente atendimento e serviço de alta qualidade! Desde o primeiro contato, fui muito bem atendida pela equipe, que demonstrou profissionalismo e dedicação. O ambiente é agradável, os produtos são de primeira qualidade e a experiência foi impecável. Recomendo a todos."
+    ],
+    "stars": 5
+  }
+]
 
   function goPrev(): void{
     swiperRef.value && swiperRef.value.slidePrev()
